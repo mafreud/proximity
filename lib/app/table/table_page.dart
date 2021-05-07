cc6102d29ace6887b3f154f07b86747e741598c6
Masahiro Aoki <rafe.kun@gmail.com>
2021-05-07 23:48:36 +0900

    refactor

--- lib/app/table/table_page.dart
+++ lib/app/table/table_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:proximity/app/customized_widgets/custom_app_bar.dart';
 
 class TablePage extends StatefulWidget {
