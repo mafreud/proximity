2134b1ffdbcbf25aae1d5232a3c09afdae736199
Masahiro Aoki <rafe.kun@gmail.com>
2021-05-12 22:20:47 +0900

    fix

--- lib/app/firebase/firebase_cloud_messaging/fcm_service.dart
+++ lib/app/firebase/firebase_cloud_messaging/fcm_service.dart
@@ -1,12 +1,20 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:proximity/app/auth/auth_service.dart';
+import 'package:proximity/app/firebase/cloud_firestore/cloud_firestore_service.dart';
+import 'package:proximity/app/firebase/cloud_firestore/firestore_path.dart';
 
 final fcmServiceProvider = Provider<FCMService>((ref) {
-  return FCMService();
+  return FCMService(
+      ref.watch(cloudFirestoreServiceProvider), ref.watch(authServiceProvider));
 });
 
 class FCMService {
+  FCMService(this._cloudFirestoreService, this._authService);
+
   final FirebaseMessaging _messaging = FirebaseMessaging.instance;
+  final CloudFirestoreService _cloudFirestoreService;
+  final AuthService _authService;
 
   Future<void> requestPermission() async {
     final settings = await _messaging.requestPermission(
@@ -21,4 +29,13 @@ class FCMService {
 
     print('User granted permission: ${settings.authorizationStatus}');
   }
+
+  Future<String?> get fcmToken async => await _messaging.getToken();
+
+  // Future<void> saveTokenToFirestore() async {
+  //   final token = fcmToken;
+  //   await _cloudFirestoreService.setData(
+  //       path: FirestorePath.fcmTokenPath(_authService.currentUserId),
+  //       data: {'token': token});
+  // }
 }
