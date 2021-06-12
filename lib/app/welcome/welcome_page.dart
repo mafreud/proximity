e87f417ff9ac25e93678c8c7656f89a0462e222c
Masahiro Aoki <rafe.kun@gmail.com>
2021-06-12 14:14:15 +0900

    add modal progress hud to signin

--- lib/app/welcome/welcome_page.dart
+++ lib/app/welcome/welcome_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_progress_hud/flutter_progress_hud.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -10,59 +11,54 @@ class WelcomePage extends ConsumerWidget {
   Widget build(BuildContext context, ScopedReader watch) {
     final screenWidth = MediaQuery.of(context).size.width;
     final viewModel = watch(welcomeViewModelProvider);
-    return Scaffold(
-      backgroundColor: Color(0xFF202124),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(40.0),
-              child: Image.asset(
-                'assets/images/app_icon.png',
-                width: screenWidth / 2,
-              ),
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.blue,
+    return ProgressHUD(
+      child: Builder(builder: (context) {
+        return Scaffold(
+          backgroundColor: Color(0xFF202124),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(40.0),
+                  child: Image.asset(
+                    'assets/images/app_icon.png',
+                    width: screenWidth / 2,
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue,
 
-                elevation: 3,
-                minimumSize: Size(110, 37), //////// HERE
-              ),
-              onPressed: () async {
-                await viewModel.signUp();
-                await Navigator.pushReplacement(context,
-                    MaterialPageRoute(builder: (context) => HomePage()));
-              },
-              child: Text('Get Started'),
+                    elevation: 3,
+                    minimumSize: Size(110, 37), //////// HERE
+                  ),
+                  onPressed: () async {
+                    final progress = ProgressHUD.of(context);
+                    progress!.show();
+                    await viewModel.signUp();
+                    await Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => HomePage()));
+                    progress.dismiss();
+                  },
+                  child: Text('Get Started'),
+                ),
+                SizedBox(height: 20),
+                InkWell(
+                  onTap: () {
+                    _launchURL(
+                        'https://www.privacypolicies.com/live/08ba4ed6-16f0-4327-b25a-3c4b2ba463fa');
+                  },
+                  child: const Text(
+                    'By Tapping Get Started \nyou are agreeing to the Terms and Conditions.',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
             ),
-            // ElevatedButton(
-            //   style: ElevatedButton.styleFrom(
-            //     primary: Colors.white10,
-            //     elevation: 3,
-            //     minimumSize: Size(110, 37), //////// HERE
-            //   ),
-            //   onPressed: () {},
-            //   child: Text(
-            //     'Login',
-            //     style: TextStyle(color: Colors.blue),
-            //   ),
-            // ),
-            SizedBox(height: 20),
-            InkWell(
-              onTap: () {
-                _launchURL(
-                    'https://www.privacypolicies.com/live/08ba4ed6-16f0-4327-b25a-3c4b2ba463fa');
-              },
-              child: const Text(
-                'By Tapping Get Started \nyou are agreeing to the Terms and Conditions.',
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      }),
     );
   }
 
