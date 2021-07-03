b15305fa1a783384907553f041ee089598bd4de9
Masahiro Aoki <rafe.kun@gmail.com>
2021-07-03 16:21:15 +0900

    package info

---
+++ lib/app/package_info_plus/package_info_plus_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+
+import '../customized_widgets/custom_app_bar.dart';
+
+class PackageInfoPlusPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(
+        title: 'PackageInfoPlus',
+        documentUrl:
+            'https://plus.fluttercommunity.dev/docs/package_info_plus/usage',
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            await _showDialog(context, await PackageInfo.fromPlatform());
+          },
+          child: Text('Get Package Infomation'),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _showDialog(BuildContext context, PackageInfo info) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Package Info Plus'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('buildNumber ${info.buildNumber}'),
+                Text('packageName: ${info.packageName}'),
+                Text('version: ${info.version}'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
